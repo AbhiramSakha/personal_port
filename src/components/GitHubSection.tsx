65ee2b6c90e7b797b51747f5b3da6343dbc5a125
AbhiramSakha <143825001+AbhiramSakha@users.noreply.github.com>
2026-03-12 15:01:19 +0530

    Initial Commit

--- src/components/GitHubSection.tsx
+++ src/components/GitHubSection.tsx
@@ -150,7 +150,7 @@ const GitHubSection = () => {
           {/* GitHub Streak Stats */}
         <div className="mt-4">
           <img
-            src={'https://streak-stats.demolab.com?user=${GITHUB_USERNAME}&theme=transparent&hide_border=true&ring=34d399&fire=34d399&currStreakLabel=34d399'}
+            src={`https://streak-stats.demolab.com?user=${GITHUB_USERNAME}&theme=transparent&hide_border=true&ring=34d399&fire=34d399&currStreakLabel=34d399`}
             alt="GitHub Streak Stats"
             className="w-full rounded-lg"
             loading="lazy"
