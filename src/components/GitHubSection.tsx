fccdb8867e23ca2ed43c34dab8c557fb70b03084
AbhiramSakha <143825001+AbhiramSakha@users.noreply.github.com>
2026-03-17 13:25:09 +0530

    Update GitHubSection.tsx

--- src/components/GitHubSection.tsx
+++ src/components/GitHubSection.tsx
@@ -140,7 +140,7 @@ const GitHubSection = () => {
             {/* GitHub Stats Card */}
           <div className="mt-6">
             <img
-              src={"https://github-readme-stats.vercel.app/api?username=AbhiramSakha&theme=tokyonight&show_icons=true&hide_border=true&count_private=true&include_all_commits=true"}
+              src={"https://github-profile-trophy.vercel.app/?username=AbhiramSakha&theme=tokyonight&no-frame=true&margin-w=10"}
               alt="GitHub stats"
               className="w-full rounded-lg"
               loading="lazy"
